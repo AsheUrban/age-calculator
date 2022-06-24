01c9148191fd9def00f3e7243c761eb586a231ce
Ashe Urban <ashe@goldentongue.com>
2022-06-24 13:04:08 -0700

    WIP GREEN & RED PHASES for test 1.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,13 +1,39 @@
 // import $ from "jquery";
 
 export default function calculatePlanetAge() {
-  if(planet === "Mercury") {
-   newPlanetAge = (earthAge * .24);
-    return newPlanetAge;
+  let newPlanetAge = [0];
+  for (let index = 0; index <= parseInt(earthAge); index += 1) {
+    newPlanetAge.push(index)
+     if(planet === "Mercury") {
+       newPlanetAge = (earthAge * mercPlanetYr);
   }
-  
+  return newPlanetAge;
+  }
 }
 
-let newPlanetAge = 0;
 let planet = "Mercury";
-const earthAge = 12;
+let mercPlanetYr = .24;
+let earthAge = 12;
+
+// let planets = ["Mercury", "Venus", "Mars", "Jupiter"];
+// const venPlanetYr = .62;
+// const marsPlanetYr = 1.88;
+// const jupPlanetYr = 11.86;
+
+// export default function calculatePlanetAge() {
+//   let newPlanetAge = [0];
+//   for (let index = 0; index <= parseInt(earthAge); index += 1) {
+//     newPlanetAge.push(index)
+//      if(planet === "Mercury") {
+//        newPlanetAge = (earthAge * mercPlanetYr);
+//         } if (planet === "Venus") {
+//           newPlanetAge = (earthAge * venPlanetYr);
+//             } if (planet === "Mars") {
+//               newPlanetAge = (earthAge * marsPlanetYr);
+//                 } if (planet === "Jupiter") {
+//                   newPlanetAge = (earthAge * jupPlanetYr);
+//                     }
+
+//     }
+//   return newPlanetAge;
+// }
