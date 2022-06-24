49a30a21ceb88eee44a8e8ccc8b8842519e03d9a
Ashe Urban <ashe@goldentongue.com>
2022-06-24 10:20:44 -0700

    RED PHASE test 1.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,9 +3,10 @@ import Calculator from '../src/js/calculator.js';
 
 describe("CalculatePlanetAge", () => {
 
-  test("it should multiply earthAge * planetYr", () => {
+  test("it should calculate age on different planet", () => {
     let earthAge = 12;
     let planetYr = .24;
+    const newPlanetAge = 0;
     expect(newPlanetAge.CalulatePlanetAge()).toEqual(earthAge * planetYr);
   })
 
