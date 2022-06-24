5dd42ffcb174e5d550da147742b215aed918ef18
Ashe Urban <ashe@goldentongue.com>
2022-06-24 08:58:38 -0700

    ADD initial project files and set up environment (TOOK FOREVER).

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,10 @@
+import Calculator from '../src/js/calculator.js';
+
+
+// describe("exampleTestFunction", () => {
+
+//   test("should....", () => {
+//     expect(exampleTestFunction()).toEqual("hi there, from the deck")
+//   });
+
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/ui-func.js',
+  entry: "./src/calculator.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
-      template: './src/index.html',
-      inject: 'body'
+      title: "Space Age Calculator",
+      template: "./src/index.html",
+      inject: "body"
     })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader'
+        loader: "eslint-loader"
       },
       {
         test: /\.(gif|png|avif|jpe?g)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
+              name: "[name].[ext]",
+              outputPath: "assets/images/"
             }
           }
         ]
       },
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ["html-loader"]
       }
     ]
   }
