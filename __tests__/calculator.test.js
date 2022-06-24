fb985046ff7333b0d740e2af30f1ec517db3ceb1
Ashe Urban <ashe@goldentongue.com>
2022-06-24 11:06:50 -0700

    FIX configuration and paths.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,13 +1,10 @@
-import Calculator from '../src/js/calculator.js';
+import calculatePlanetAge from '../src/js/calculator.js';
 
 
-describe("CalculatePlanetAge", () => {
+describe("calculatePlanetAge", () => {
 
-  test("it should calculate age on different planet", () => {
-    let earthAge = 12;
-    let planetYr = .24;
-    const newPlanetAge = 0;
-    expect(newPlanetAge.CalulatePlanetAge()).toEqual(earthAge * planetYr);
+  test("it should calculate age on different planets", () => {
+    expect(newPlanetAge.calulatePlanetAge()).toEqual();
   })
 
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,8 +1,16 @@
 // import $ from "jquery";
 
-//  const exampleTestFunction = () => {
-//   console.log("hi there, from the deck");
-//   return "hi there, from the deck";
-// };
 
-// export default exampleTestFunction;
+
+
+
+export default function calculatePlanetAge(earthAge, planetYr) {
+  if(planet === "Mercury") {
+    (earthAge * planetYr);
+  }
+}
+
+let planet = "Mercury";
+const planetYr = .24;
+const earthAge = 80;
+let newPlanetAge = 0;
